b9009c11c1142e3b6915b2eefd33ed994e9a49eb
Raul Cote <raulcote@MacBook-Air-de-Raul.local>
2026-08-12 18:02:09 +0200

    Corregir dorsales de Connor y Ángel

--- src/utils/roster.ts
+++ src/utils/roster.ts
@@ -2,8 +2,8 @@ import type { Player } from '../types';
 
 const shirtNumbers: Array<[number, RegExp]> = [
   [1, /^álvaro torres$/i], [2, /^jairo muñoz$/i], [3, /^antonio torres$/i], [4, /^daren castro$/i],
-  [5, /^jorge vera$/i], [6, /^raúl andrades$/i], [7, /^omar(?:\s|$)/i], [8, /^samuel lozano$/i],
-  [9, /cañestro/i], [10, /palomo/i], [11, /^sam( russell)?$/i], [13, /^dani/i],
+  [5, /^jorge vera$/i], [6, /^raúl andrades$/i], [7, /^connor(?:\s|$)/i], [8, /^samuel lozano$/i],
+  [9, /cañestro/i], [10, /^ángel omaña$/i], [11, /^sam( russell)?$/i], [13, /^dani/i],
   [14, /^manuel arroyo$/i], [16, /^samuel (espinosa|m)/i], [17, /piñer/i], [18, /^(cristian|kristian) blanco$/i],
   [19, /parra/i], [20, /(álex|alex).*herrera|a\. herrera/i], [21, /oncala/i], [22, /^nico/i],
   [23, /^pepe ruiz$/i], [24, /^francis pecino$/i], [25, /^álvaro cervera$/i], [26, /^lázaro gómez$/i],
